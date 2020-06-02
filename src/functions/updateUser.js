9df32edd20b4c0658f5909b8e15083aba185ab40
Roger Keith <rogerdkeith@outlook.com>
2020-06-02 16:16:13 -0700

    Adjust updateUser to async/await

--- src/functions/updateUser.js
+++ src/functions/updateUser.js
@@ -1,32 +1,49 @@
 'use strict';
 const AWS = require('aws-sdk');
 const db = require('../../database/dynamodb');
-const response = require('../../helpers/response');
 
 const usersTable = process.env.DYNAMODB_TABLE;
 
-module.exports.updateUser = (event, context, callback) => {
-  const { city, username } = event.pathParameters;     // Change with Slack Integration?
-  const body = JSON.parse(event.body);
-  //const paramName = body.paramName;
-  //const paramValue = body.paramValue;
-  const deliveryDay = body.deliveryDay;
-
+// async function abstraction
+async function updateItem(city, username, deliveryDay) {
   const params = {
     TableName: usersTable,
     Key: {
-      city: city,
-      username: username
+      city,
+      username
     },
     ConditionExpression: 'attribute_exists(city) AND attribute_exists(username)',
-    UpdateExpression: 'set deliveryDay = :v',                //How to set this to be anything with Slack Form?
+    UpdateExpression: 'set deliveryDay = :v',                //How to set this to be anything?
     ExpressionAttributeValues: {
       ':v': deliveryDay
     },
     ReturnValues: 'ALL_NEW'
-  };
+  }
+  try {
+    await db.update(params).promise()
+  } catch (err) {
+    return err
+  }
+}
 
-  return db.update(params).promise().then(res => {
-    callback(null, response(200, res.Attributes))
-  }).catch(err => response(null, response(err.statusCode, err)));
+// usage
+exports.updateUser = async (event, context) => {
+  try {
+    const { city, username, deliveryDay } = JSON.parse(event.body);
+
+    const response = await updateItem(city, username, deliveryDay)
+    if (response) {
+      return { 
+        statusCode: response.statusCode,
+        body: JSON.stringify(response)
+      }
+    } else {
+      return {
+        statusCode: 200,
+        body: "User updated successfully!"
+      }
+    }
+  } catch (err) {
+        return { error: err }
+    }
 }
