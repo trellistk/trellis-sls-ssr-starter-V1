7ce967a8d3bad0ee88022d0bb7a9c7ac73f93943
Amber Kim <ambergkim@gmail.com>
2020-06-17 19:52:16 -0700

    test

--- helpers/db.js
+++ helpers/db.js
@@ -6,12 +6,12 @@ const usersTable = process.env.DYNAMODB_TABLE
 module.exports.createItem = async (data) => {
   var params = {
     TableName: usersTable,
-    Item: itemData,
+    Item: data,
     ConditionExpression: 'attribute_not_exists(username)'
   }
   try {
-    const data = await db.put(params).promise()
-    return { data }
+    const response = await db.put(params).promise()
+    return { data: response }
   } catch (error) {
     return { error }
   }
