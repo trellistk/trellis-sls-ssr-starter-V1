afd2b9553e6642572ec056afea1c57d756e1c8c8
Roger Keith <rogdkeith@gmail.com>
2020-11-28 21:51:34 -0600

    Add local tests for updating email

---
+++ src/tests/local/updateEmail.test.js
@@ -0,0 +1,117 @@
+'use strict'
+
+require('dotenv').config()
+
+const API_DOMAIN_LOCAL = process.env.API_DOMAIN_LOCAL
+
+const test = require('tape')
+const offline = require('../test-utils/offline')
+const fetch = require('node-fetch')
+const { userFactory } = require('../test-utils/data_factories')
+
+test('Happy path updating a user email', async t => {
+  await offline.start()
+
+  await fetch(`${API_DOMAIN_LOCAL}/signup`, {
+    method: 'POST',
+    body: JSON.stringify(userFactory.correct)
+  })
+
+  await fetch(`${API_DOMAIN_LOCAL}/login`, {
+    method: 'POST',
+    body: JSON.stringify({
+      email: userFactory.correct.email,
+      password: userFactory.correct.password,
+      chapter: userFactory.correct.chapter
+    })
+  })
+
+  const res = await fetch(`${API_DOMAIN_LOCAL}/update/email`, {
+    method: 'POST',
+    body: JSON.stringify({
+      email: userFactory.emailUpdate.newEmail
+    })
+  })
+
+  const json = await res.json()
+  const expectedBody = 'User email successfully updated!'
+
+  t.equals(res.status, 201, 'Returns http 201')
+  t.equals(json.message, expectedBody, 'Returns correct response body')
+
+  await offline.stop()
+  t.end()
+})
+
+test(`Should not allow email update with old email`, async t => {
+  await offline.start()
+
+  await fetch(`${API_DOMAIN_LOCAL}/signup`, {
+    method: 'POST',
+    body: JSON.stringify(userFactory.correct)
+  })
+
+  await fetch(`${API_DOMAIN_LOCAL}/login`, {
+    method: 'POST',
+    body: JSON.stringify({
+      email: userFactory.correct.email,
+      password: userFactory.correct.password,
+      chapter: userFactory.correct.chapter
+    })
+  })
+
+  const res = await fetch(`${API_DOMAIN_LOCAL}/update/email`, {
+    method: 'POST',
+    body: JSON.stringify({
+      email: userFactory.correct.email
+    })
+  })
+
+  const json = await res.json()
+  const expectedBody = 'New email cannot match old email'
+
+  t.equals(res.status, 400, 'Returns http 400')
+  t.equals(json.message, expectedBody, 'Returns correct response body')
+
+  await offline.stop()
+  t.end()
+})
+
+test('Should not allow email update with non-unique new email', async t => {
+  await offline.start()
+
+  await fetch(`${API_DOMAIN_LOCAL}/signup`, {
+    method: 'POST',
+    body: JSON.stringify(userFactory.correct)
+  })
+
+  await fetch(`${API_DOMAIN_LOCAL}/signup`, {
+    method: 'POST',
+    body: JSON.stringify(userFactory.correct2)
+  })
+
+  await fetch(`${API_DOMAIN_LOCAL}/login`, {
+    method: 'POST',
+    body: JSON.stringify({
+      email: userFactory.correct.email,
+      password: userFactory.correct.password,
+      chapter: userFactory.correct.chapter
+    })
+  })
+
+  const res = await fetch(`${API_DOMAIN_LOCAL}/update/email`, {
+    method: 'POST',
+    body: JSON.stringify({
+      email: userFactory.correct2.email
+    })
+  })
+
+  const json = await res.json()
+  const expectedBody = 'ERROR_USER_EXISTS'
+
+  t.equals(res.status, 403, 'Returns http 403')
+  t.equals(json.message, expectedBody, 'Returns correct response body')
+
+  await offline.stop()
+  t.end()
+})
--- src/tests/test-utils/data_factories.js
+++ src/tests/test-utils/data_factories.js
@@ -31,5 +31,24 @@ module.exports.userFactory = {
     oldPassword: '123password&23845',
     newPassword1: 'simple123!!',
     newPassword2: 'simple123!!'
-  }
+  },
+  emailUpdate: {
+    newEmail: 'john@doe.com'
+  },
+  correct2: {
+    chapter: 'seattle-eastside',
+    email: 'john@doe.com',
+    password: '123password&23845',
+    fname: 'john',
+    lname: 'doe',
+    phone: '1234567890',
+    street1: '123 main st apt 6',
+    // street2: null,
+    city: 'seattle',
+    state: 'wa',
+    zip: '98119',
+    totalHouseholdIncome: 1500
+    // communityAlias: null,
+    // deliveryNotes: null
+  },
 }
