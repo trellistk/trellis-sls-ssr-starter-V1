c73068ebafd06ff35331385818818e85a34b03b0
Jay Palacio <jaypalacio@gmail.com>
2022-02-26 16:03:40 -0500

    add .husky, change ci/cd flows, remove tests

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,7 +1,7 @@
-"use strict";
+'use strict'
 
-const { render, response } = require("simple-sls-ssr");
+const { render } = require('simple-sls-ssr')
 
 module.exports.index = async (event, context) => {
-  return await render("index", {});
-};
+  return await render('index', {})
+}
